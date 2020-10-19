56e3be85ecd12a94dd6ebb878f3c1c3b1ac54629
Rachel Braband <rebraband@gmail.com>
2020-10-19 14:56:46 -0500

    commit

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -34,7 +34,7 @@ class Courses extends Component {
     if(this.state.courses.length > 0) {
         courses = this.state.courses.map((course) => 
             <div className="grid-33" key={course.id}>
-                <a className="course--module course--link" href={'api/courses/' + course.id}>
+                <a className="course--module course--link" href={'/courses/' + course.id}>
                     <h4 className="course--label">Course</h4>
                     <h3 className="course--title">{course.title}</h3>
                 </a>
@@ -46,7 +46,7 @@ class Courses extends Component {
         return (
             <div className="bounds">
                 {courses}
-                <div className="grid-33"><a className="course--module course--add--module" href="api/courses/create">
+                <div className="grid-33"><a className="course--module course--add--module" href="/courses/create">
             <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
                 viewBox="0 0 13 13" className="add">
                 <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
